1fdb665af3bdcbe188d34e0e9a8672cd458241ba
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-16 11:31:05 +0100

    fix: fix route problems

--- client/src/components/home/Header.jsx
+++ client/src/components/home/Header.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { assets } from '../../assets/assets.js';
 import { AppContent } from '../../context/AppContext.jsx';
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
   const { userData } = useContext(AppContent);
+  const navigate = useNavigate()
   return (
     <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
       <img
@@ -20,13 +22,13 @@ const Header = () => {
         />
       </h1>
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>
-        Welcome to our app
+        Welcome to Pulsetracker
       </h2>
       <p className='mb-8 max-w-md'>
         Let's start with a quick product tour and we will have you up and
         running in no time!
       </p>
-      <button className='cursor-pointer border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>
+      <button onClick={() => navigate('/dashboard')} className='cursor-pointer border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>
         Get started
       </button>
     </div>
